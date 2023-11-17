803ff33547a8f67f7c57284053c4144727441699
unknown <arpit.sahu@bridgelabz.com>
2023-11-17 12:05:24 +0530

    Added the menu based approcah to the programme

--- src/Main.java
+++ src/Main.java
@@ -15,22 +15,26 @@ public class Main{
     }
     public static void main(String[] args){
         System.out.println("Welcome to calculator");
-
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your 1st number");
-        double var1 = sc.nextDouble();
-        System.out.println("Enter your 2nd number");
-        double var2 = sc.nextDouble();
-        System.out.println("Enter your choice\n1:Addition\n2:Subtraction\n3:Multiplication\n4:Division ");
-        int choice = sc.nextInt();
-        double var3;
-        switch (choice) {
-            case 1 -> System.out.println("Addition of your input is :" + addition(var1, var2));
-            case 2 -> System.out.println("Addition of your input is :" + subtraction(var1, var2));
-            case 3 -> System.out.println("Addition of your input is :" + multiplication(var1, var2));
-            case 4 -> System.out.println("Addition of your input is :" + division(var1, var2));
-            default -> System.out.println("Invalid option");
-        }
+        int option=0;
+        do{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your 1st number");
+            double var1 = sc.nextDouble();
+            System.out.println("Enter your 2nd number");
+            double var2 = sc.nextDouble();
+            System.out.println("Enter your choice\n1:Addition\n2:Subtraction\n3:Multiplication\n4:Division ");
+            int choice = sc.nextInt();
+            double var3;
+            switch (choice) {
+                case 1 -> System.out.println("Addition of your input is :" + addition(var1, var2));
+                case 2 -> System.out.println("Addition of your input is :" + subtraction(var1, var2));
+                case 3 -> System.out.println("Addition of your input is :" + multiplication(var1, var2));
+                case 4 -> System.out.println("Addition of your input is :" + division(var1, var2));
+                default -> System.out.println("Invalid option");
+            }
+            System.out.println("To exit the programme press 1");
+            option = sc.nextInt();
+        }while (option!=1);
     }
+
 }
