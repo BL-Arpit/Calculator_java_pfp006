9d8b7460b84c7fa641239bd3e6f077f9d1f14dd3
unknown <arpit.sahu@bridgelabz.com>
2023-11-24 11:42:51 +0530

    Added Advanced for each loop

---
+++ src/DemoForEaach.java
@@ -0,0 +1,9 @@
+public class DemoForEaach {
+    public static void main(String[] args) {
+        Integer[] arr1 = {10, 12, 16, 90, 500};
+        for (Integer i: arr1){
+            System.out.println(i);
+        }
+
+    }
+}
